9399431d1fc6e6723cc8e2d8b9131b993bdd3c0e
Anders Børjesson <anbo@zealand.dk>
2023-09-13 13:32:43 +0200

    Add project files.

---
+++ SchoolLib/Teacher.cs
@@ -0,0 +1,30 @@
+﻿namespace SchoolLib
+{
+    public class Teacher
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int Salary { get; set; }
+
+        
+        public void ValidateName()
+        {
+            if (Name == null)
+            {
+                throw new ArgumentNullException("Name");
+            }
+            if (Name == "")
+            {
+                throw new ArgumentException("Name cannot be empty", "Name");
+            }
+
+        }
+
+        public void Validate()
+        {
+            {
+                ValidateName();
+            }
+        }
+    }
+}
---
+++ SchoolLibTests/TeacherTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SchoolLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolLib.Tests
+{
+    [TestClass()]
+    public class TeacherTests
+    {
+        [TestMethod()]
+        public void ValidateNameTest()
+        {
+            Teacher anders = new Teacher() { Id = 1, Name = "A", Salary = 0 };
+            anders.ValidateName();
+
+            Teacher nullName = new Teacher() { Id = 1, Name = null, Salary = 0 };
+            Assert.ThrowsException<ArgumentNullException>(
+                () => nullName.ValidateName());
+
+        }
+
+        [TestMethod()]
+        public void ValidateSalaryTest()
+        {
+
+            Teacher anders = new Teacher() { Id = 1, Name = "Anders", Salary = 0 };
+            // anders.ValidateSalary();
+
+            Teacher negativeSalary = new Teacher() { Id = 1, Name = "Anders", Salary = -1 };
+            // Assert.ThrowsException<ArgumentOutOfRangeException>(
+            //     () => negativeSalary.ValidateSalary());
+
+
+        }
+    }
+}
